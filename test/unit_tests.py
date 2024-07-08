a427aa14827db8e56e1f5448e77460cf240e1219
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-07-08 16:45:26 -0500

    added tests for determinant

--- test/unit_tests.py
+++ test/unit_tests.py
@@ -41,5 +41,29 @@ class TestLinAlg(unittest.TestCase):
     def test_list_dot_product_neg(self):
         self.assertEqual(linalg.list_dot_product([1, 2, 3], [-4, -5, -6]), -32)
 
+    # determinant(matrix)
+    def test_determinant_1(self):
+        self.assertEqual(linalg.determinant([[1]]), 1)
+    
+    # det(I) = 1
+    def test_determinant_2_identity(self):
+        self.assertEqual(linalg.determinant([[1, 0], [0, 1]]), 1)
+
+    # det with zero row/col = 0
+    def test_determinant_2_zero_row(self):
+        self.assertEqual(linalg.determinant([[2, 2], [0, 0]]), 0)
+    def test_determinant_2_zero_col(self):
+        self.assertEqual(linalg.determinant([[2, 0], [2, 0]]), 0)
+    def test_determinant_2_1(self):
+        self.assertEqual(linalg.determinant([[8, 6], [3, 4]]), 14)
+
+    # det(L) = det(U) = diagonal product
+    def test_determinant_3_upper_triangular(self):
+        self.assertEqual(linalg.determinant([[3, 1, 1], [0, 2, 5], [0, 0, 4]]), 24)
+    def test_determinant_3_lower_triangular(self):
+        self.assertEqual(linalg.determinant([[4, 0, 0], [5, 2, 0], [1, 1, 3]]), 24)
+    def test_determinant_3_1(self):
+        self.assertEqual(linalg.determinant([[3, 1, 1], [4, -2, 5], [2, 8, 7]]), -144)
+
 if __name__ == '__main__':
     unittest.main()
