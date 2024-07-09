f7e5dd4d3ae0314f39b2b3064b08a3e358d60f5e
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-07-09 16:25:29 -0500

    restructured tests for det

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 use pyo3::prelude::*;
 use pyo3::types::{PyList, PyFloat};
 use pyo3::exceptions::PyValueError;
+use matrix::matrix::determinant;
+
+mod matrix {
+	pub mod matrix;
+}
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -81,63 +86,6 @@ fn list_dot_product<'py>(a: Bound<'py, PyList>, b: Bound<'py, PyList>) -> PyResu
     Ok(sum)
 }
 
-/// determinant
-/// TODO dimensions check
-/// TODO generics + type constraint
-/// TODO allow for larger matrices
-#[pyfunction]
-fn determinant<'py>(matrix: Vec<Vec<f64>>) -> PyResult<f64> {
-    let mut det: f64 = 0.0;
-    let n = matrix.len();
-    if n == 1 {
-        det = matrix[0][0];
-        return Ok(det);
-    }
-    if n == 2 {
-        det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
-        return Ok(det);
-    }
-
-    //     a b c   aei   afh
-    // det d e f = bfg - bdi
-    //     g h i   cdh   ceg
-    for start in 0..n {
-        let mut prod_add = 1.0;
-        let mut prod_sub = 1.0;
-        
-        let mut r_add = 0;
-        let mut c_add = start;
-        
-        let mut r_sub = 0;
-        let mut c_sub = start;
-
-        for _it in 0..n {
-            prod_add *= matrix[r_add][c_add];
-            r_add += 1;
-            c_add += 1;
-            if c_add == n {
-                c_add = 0;
-            }
-
-            prod_sub *= matrix[r_sub][c_sub];
-            r_sub += 1;
-            if c_sub == 0 {
-                c_sub = n - 1;
-            } else {
-                c_sub -= 1;
-            }
-        }
-
-        //println!("diagonal + = {}", prod_add);
-        //println!("diagonal - = {}", prod_sub);
-
-        det += prod_add;
-        det -= prod_sub;
-    }
-
-    Ok(det)
-}
-
 /// A Python module implemented in Rust.
 #[pymodule]
 fn linalg(m: &Bound<'_, PyModule>) -> PyResult<()> {
@@ -145,6 +93,8 @@ fn linalg(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(list_to_string, m)?)?;
     m.add_function(wrap_pyfunction!(scale_list, m)?)?;
     m.add_function(wrap_pyfunction!(list_dot_product, m)?)?;
+	
+	// matrix
     m.add_function(wrap_pyfunction!(determinant, m)?)?;
     Ok(())
 }
--- test/unit_tests.py
+++ test/unit_tests.py
@@ -42,28 +42,38 @@ class TestLinAlg(unittest.TestCase):
         self.assertEqual(linalg.list_dot_product([1, 2, 3], [-4, -5, -6]), -32)
 
     # determinant(matrix)
-    def test_determinant_1(self):
-        self.assertEqual(linalg.determinant([[1]]), 1)
-    
+    def test_determinant(self):
+        self.assertEqual(linalg.determinant([[3.14]]), 3.14)
+        self.assertEqual(linalg.determinant([[8, 6], [3, 4]]), 14)
+        self.assertEqual(linalg.determinant([[3, 1, 1], [4, -2, 5], [2, 8, 7]]), -144)
+        self.assertEqual(linalg.determinant([
+            [9.8656, 10.0306, 0.3472, 11.0069, 7.3139, 4.4606, 4.2002],
+            [2.0655, 14.5462, 0.9602, 13.7961, 2.8015, 9.0956, 5.0173],
+            [5.2817, 14.3850, 9.1309, 10.1272, 5.2193, 2.4805, 2.9069],
+            [6.0149, 14.8712, 7.1108, 3.9310, 10.5918, 0.5560, 11.3649],
+            [4.4826, 7.7488, 12.8341, 9.5523, 6.5164, 9.0212, 9.4849],
+            [6.6361, 12.5463, 2.7712, 1.9945, 7.0052, 7.3419, 12.4871],
+            [4.0405, 3.5168, 1.0304, 9.5555, 2.0069, 12.1179, 1.0339]]), 3.7106e6)
     # det(I) = 1
-    def test_determinant_2_identity(self):
+    def test_determinant_identity(self):
+        self.assertEqual(linalg.determinant([[1]]), 1)
         self.assertEqual(linalg.determinant([[1, 0], [0, 1]]), 1)
+        self.assertEqual(linalg.determinant([[1, 0, 0], [0, 1, 0], [0, 0, 1]]), 1)
 
     # det with zero row/col = 0
     def test_determinant_2_zero_row(self):
         self.assertEqual(linalg.determinant([[2, 2], [0, 0]]), 0)
+        self.assertEqual(linalg.determinant([[2, 2, 2, 2, 2], [0, 0, 0, 0, 0], [2, 2, 2, 2, 2], [2, 2, 2, 2, 2], [2, 2, 2, 2, 2]]), 0)
     def test_determinant_2_zero_col(self):
         self.assertEqual(linalg.determinant([[2, 0], [2, 0]]), 0)
-    def test_determinant_2_1(self):
-        self.assertEqual(linalg.determinant([[8, 6], [3, 4]]), 14)
 
     # det(L) = det(U) = diagonal product
-    def test_determinant_3_upper_triangular(self):
+    def test_determinant_triangular(self):
         self.assertEqual(linalg.determinant([[3, 1, 1], [0, 2, 5], [0, 0, 4]]), 24)
-    def test_determinant_3_lower_triangular(self):
         self.assertEqual(linalg.determinant([[4, 0, 0], [5, 2, 0], [1, 1, 3]]), 24)
-    def test_determinant_3_1(self):
-        self.assertEqual(linalg.determinant([[3, 1, 1], [4, -2, 5], [2, 8, 7]]), -144)
+        
+        
+
 
 if __name__ == '__main__':
     unittest.main()
