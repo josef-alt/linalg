6f8490bbb8772e5b4cb977e69181c1b74bce1be6
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2024-07-11 18:18:24 -0500

    removed tests for old vector function

--- test/unit_tests.py
+++ test/unit_tests.py
@@ -2,33 +2,6 @@ import unittest
 import linalg
 
 class TestLinAlg(unittest.TestCase):
-
-    # sum_as_string(a, b)
-    def test_sum_as_string_zero(self):
-        self.assertEqual(linalg.sum_as_string(0, 0), "0")
-    def test_sum_as_string_pos(self):
-        self.assertEqual(linalg.sum_as_string(1, 10), "11")
-
-    # list_to_string(list)
-    def test_list_to_string_empty(self):
-        self.assertEqual(linalg.list_to_string([]), "[]")
-    def test_list_to_string(self):
-        self.assertEqual(linalg.list_to_string([1, 2, 3]), "[1, 2, 3]")
-
-    # scale_list(list, scalar)
-    def test_scale_list_empty(self):
-        test = []
-        linalg.scale_list(test, 10)
-        self.assertEqual(test, [])
-    def test_scale_list_zero(self):
-        test = [1, 2, 3]
-        linalg.scale_list(test, 0)
-        self.assertEqual(test, [0, 0, 0])
-    def test_scale_list(self):
-        test = [1, 2, 3]
-        linalg.scale_list(test, 2)
-        self.assertEqual(test, [2, 4, 6])
-
     # list_dot_product(list1, list2)
     def test_list_dot_product_invalid(self):
         self.assertRaises(ValueError, lambda: linalg.list_dot_product([], [1, 2, 3]))
